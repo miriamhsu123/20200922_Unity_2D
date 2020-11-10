cd3efe48d4449a40d4d0eb2712d553f4b665ef04
miriamhsu123 <71687830+miriamhsu123@users.noreply.github.com>
2020-11-10 16:09:36 +0800

    問題有夠多 我要哭

---
+++ 20200922_A305_2D_game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager  : MonoBehaviour
+{
+   public void NextLevel()
+    {
+
+    }
+
+    public void backtomenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+    public void quit()
+    { 
+    
+    }
+   
+}
--- 20200922_A305_2D_game/Assets/Scripts/Player.cs
+++ 20200922_A305_2D_game/Assets/Scripts/Player.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject congratulation;
     public GameObject failure;
+    public Text textCount;
+    public int count;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -11,6 +14,12 @@ public class Player : MonoBehaviour
         print("觸及傳送門");
         congratulation.SetActive(true);
 
-    }
 
+    if(collision.tag == "cherry")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "Coin:" + count;
+        }
+    }
 }
